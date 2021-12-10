9c2f69bbf1ecdc892e7b89f62bbee1b6790ded13
Sanjeet Attili <57587354+SanjuEpic@users.noreply.github.com>
2021-12-10 21:18:16 +0530

    app code updated

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from lime import lime_tabular
-from flask import Flask, render_template, render_template_string, url_for, request
+from flask import Flask, render_template, request
 import pickle, numpy as np
-from sklearn.ensemble import RandomForestClassifier
 
 
 with open('60_important_features_rf_fml.pkl', mode='rb') as f:
   fairml_features=pickle.load(f)
 
-with open('rf_fairml_60_features_model.pkl', mode='rb') as f:
+with open('rf_fml_model.pkl', mode='rb') as f:
   rf_fml=pickle.load(f)
 
 with open('class_names.pkl', mode='rb') as f:
@@ -20,9 +19,6 @@ app=Flask(__name__, template_folder='templates',
           static_folder='static')
 
 train=x_train[fairml_features]
-
-# app=Flask(__name__, template_folder='your_name_for_template_folder')
-# start ngrok when flask is made to run
 
 @app.route('/')
 def home():
@@ -37,21 +33,18 @@ def results():
     for symptom in symptoms:
       if symptom in fairml_features:
         final.append(symptom)
-    # print(final)
 
-    for f in fairml_features:
-      if f in final:
+    for feature in fairml_features:
+      if feature in final:
         test_vector.append(1)
       else:
         test_vector.append(0)
-    # print(test_vector)
 
     test_vector=np.array(test_vector)
     label=class_names[rf_fml.predict(test_vector.reshape(1,-1))]
     explainer=lime_tabular.LimeTabularExplainer(training_data=train.values, class_names=class_names, feature_names=fairml_features, categorical_features=range(len(train.columns))) #categorical_features=final_rf_features)
     exp = explainer.explain_instance(test_vector, rf_fml.predict_proba, num_features=10, top_labels=1)
     exp = exp.as_html()
-    # print(label)
     
     return render_template('results.html', explainer_=exp,  output=", ".join(symptoms), class_=label[0].title())
 
